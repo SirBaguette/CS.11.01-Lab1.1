bb346ae6c6e761b65fd00691a43a75bf9ccea2df
SirBaguette <sebastianz2026@student.cis.edu.hk>
2023-09-12 21:29:35 +0800

    .

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Dateable {
 
+
+
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = -1;
+        while (age < 0) {
+            try {
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number");
+            }
+        }
+
+
+        System.out.println("You should date someone who is at least " + (age/2 +7));
+
         
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = -1;
+        while (age < 0 || age > 140) {
+            try {
+                age = Integer.parseInt(scanner.nextLine());
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number");
+            }
+        }
+
+
+        if ((100 - age) > 1) {
+            System.out.println("You need to read at least " + (100 - age) + " pages before you give up a book");
+        }
+        else {
+            System.out.println("You need to read at least " + (1) + " page before you give up a book");
+        }
+
 
     }
 }
